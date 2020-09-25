c94f131ac36ae380f2fc33e043147ff7f94e4ceb
Timo Hyppönen <timojuhani.hypponen@gmail.com>
2020-09-26 02:10:00 +0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,9 +1,8 @@
 'use strict';
 
-
 //to do:
 
-// range
+// range, toimii osittain
 // haku
 //haulla markerin siirtyminen ja pisteiden päivittyminen siihen
 // marker merging mitä yritin, ,hyi
@@ -12,81 +11,79 @@
 
 //glhf
 
-
-
-
-
 /*let kaytto1 = "";
 let kaytto = "";*/
 let paikka = null;
 
 //eri kuvake latauspisteille
 let greenIcon = L.icon({
-    iconUrl: 'kuvat/kuvake.png',
-    shadowUrl: 'kuvat/kuvake.png',
+  iconUrl: 'kuvat/kuvake.png',
+  shadowUrl: 'kuvat/kuvake.png',
 
-    iconSize: [28, 28], // size of the icon
-    shadowSize: [0, 0], // size of the shadow
-    iconAnchor: [19, 19], // point of the icon which will correspond to marker's location
-    shadowAnchor: [4, 62],  // the same for the shadow
-    popupAnchor: [-5, -20] // point from which the popup should open relative to the iconAnchor
+  iconSize: [28, 28], // size of the icon
+  shadowSize: [0, 0], // size of the shadow
+  iconAnchor: [19, 19], // point of the icon which will correspond to marker's location
+  shadowAnchor: [4, 62],  // the same for the shadow
+  popupAnchor: [-5, -20], // point from which the popup should open relative to the iconAnchor
 });
 
 //punainen kuvake
 let defaultIcon = L.icon({
-    iconUrl: 'kuvat/default.png',
-    iconSize: [25, 41], // size of the icon
-    shadowSize: [0, 0], // size of the shadow
-    iconAnchor: [12.5, 41], // point of the icon which will correspond to marker's location
-    shadowAnchor: [12.5, 41],  // the same for the shadow
-    popupAnchor: [1, -45] // point from which the popup should open relative to the iconAnchor
+  iconUrl: 'kuvat/default.png',
+  iconSize: [25, 41], // size of the icon
+  shadowSize: [0, 0], // size of the shadow
+  iconAnchor: [12.5, 41], // point of the icon which will correspond to marker's location
+  shadowAnchor: [12.5, 41],  // the same for the shadow
+  popupAnchor: [1, -45], // point from which the popup should open relative to the iconAnchor
 });
 
 // liitetään kartta elementtiin #map
 //const map = L.map('map');
 const map = new L.Map('map', {
-    fullscreenControl: true,
-    fullscreenControlOptions: {
-        position: 'bottomleft'
+  fullscreenControl: true,
+  fullscreenControlOptions: {
+    position: 'bottomleft',
 
-    }
+  },
 });
 
 //map.addControl(control.zoom({ position: 'bottomright' }));
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> | Google tilelayers',
+  attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> | Google tilelayers',
 }).addTo(map);
 paivitaKartta({latitude: 64, longitude: 24}, 5);
 
 //Satelliittikuva tai no ns hybridi kuva
-let googleHybrid = L.tileLayer('http://{s}.google.com/vt/lyrs=s,h&x={x}&y={y}&z={z}', {
-    maxZoom: 20,
-    subdomains: ['mt0', 'mt1', 'mt2', 'mt3']
-});
+let googleHybrid = L.tileLayer(
+    'http://{s}.google.com/vt/lyrs=s,h&x={x}&y={y}&z={z}', {
+      maxZoom: 20,
+      subdomains: ['mt0', 'mt1', 'mt2', 'mt3'],
+    });
 let latauspaikatm = L.layerGroup();
 let baseMaps = {
-    //"Satelliitti": googleSat,
-    "Hybridi": googleHybrid,
-    "Normaali": map,
+  //"Satelliitti": googleSat,
+  'Hybridi': googleHybrid,
+  'Normaali': map,
 
 };
 let overlayMaps = {
 
-        "Latauspaikat": latauspaikatm
+  'Latauspaikat': latauspaikatm,
 
-}
+};
 
 //minimappi
-var osm2 = new L.TileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {minZoom: 0, maxZoom: 13});
+var osm2 = new L.TileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
+    {minZoom: 0, maxZoom: 13});
 var miniMap = new L.Control.MiniMap(osm2).addTo(map);
 
 // events are fired when entering or exiting fullscreen.
-map.on('enterFullscreen', function () {
-    console.log('entered fullscreen');
+map.on('enterFullscreen', function() {
+  console.log('entered fullscreen');
 });
 
-map.on('exitFullscreen', function () {
-    console.log('exited fullscreen');
+map.on('exitFullscreen', function() {
+  console.log('exited fullscreen');
 });
 
 L.control.layers(baseMaps, overlayMaps).addTo(map);
@@ -94,43 +91,46 @@ L.control.layers(baseMaps, overlayMaps).addTo(map);
 
 // Asetukset paikkatiedon hakua varten (valinnainen)
 const options = {
-    enableHighAccuracy: true,
-    timeout: 5000,
-    maximumAge: 0,
+  enableHighAccuracy: true,
+  timeout: 5000,
+  maximumAge: 0,
 };
 //kompassi, huutista
-var kompassi =  new L.Control.Compass().addTo(map);
+var kompassi = new L.Control.Compass().addTo(map);
 
 // Funktio, joka ajetaan, kun paikkatiedot on haettu
 function success(pos) {
-    const crd = pos.coords;
-    paikka = crd;
+  const crd = pos.coords;
+  paikka = crd;
 
-    // Tulostetaan paikkatiedot konsoliin
-    console.log('Your current position is:');
-    console.log(`Latitude : ${crd.latitude}`);
-    console.log(`Longitude: ${crd.longitude}`);
-    console.log(`More or less ${crd.accuracy} meters.`);
+  // Tulostetaan paikkatiedot konsoliin
+  console.log('Your current position is:');
+  console.log(`Latitude : ${crd.latitude}`);
+  console.log(`Longitude: ${crd.longitude}`);
+  console.log(`More or less ${crd.accuracy} meters.`);
 
-    // päivitä kartta
-    paivitaKartta(crd, 13);
+  // päivitä kartta
+  paivitaKartta(crd, 13);
 
-    // lisää marker
-    lisaaMarker(crd, "", {icon: defaultIcon});
+  // lisää marker
+  lisaaMarker(crd, '', {icon: defaultIcon});
 
+  //hae asemat
+  //haeLatauspisteet(crd);
+  /*rangeslider.oninput =*/
 
-    //hae asemat
-    //haeLatauspisteet(crd);
-    /*rangeslider.oninput =*/
-    rangeslider.oninput = function() {
-        haeLatauspisteet(crd);
-    }
+  //suorita kerran
+  haeLatauspisteet(crd);
+  //suorita uudestaan kun arvo muuttuu
+  rangeslider.oninput = function() {
+    map.removeLayer(latauspaikatm);
+    haeLatauspisteet(crd);
+  };
 }
-
 
 // funktio, joka keskittää kartan
 function paivitaKartta(crd, zoom) {
-    map.setView([crd.latitude, crd.longitude], zoom);
+  map.setView([crd.latitude, crd.longitude], zoom);
 }
 
 //a href="#" id="tismus"><div id="navigoi">Navigoi</div></a>
@@ -144,60 +144,69 @@ a.appendChild(timppa);
 */
 
 // funktio, joka tekee markerin
-function lisaaMarker(crd, latauspiste = "", kuvake, teksti = "Olen tässä.", usagecost = "", connectiontype = "", osoite = "",
-                     kaupunki = "", kordinaattix = crd.latitude, kordinaattiy = crd.latitude, tyyppi = "",
-                     acdc = "", käyttö = "", pistokkeet) {
+function lisaaMarker(crd, latauspiste = '', kuvake, teksti = 'Olen tässä.',
+                     usagecost = '', connectiontype = '', osoite = '',
+                     kaupunki = '', kordinaattix = crd.latitude,
+                     kordinaattiy = crd.latitude, tyyppi = '',
+                     acdc = '', käyttö = '', pistokkeet) {
 
+  // try {
+  L.marker([crd.latitude, crd.longitude], kuvake).
+      addTo(map).
+      bindPopup(
+          '<b>' + teksti + '</b><br> <span class="osoite">' + osoite + ', ' +
+          kaupunki + '</span><br>' +
+          '<span class=\'pienempiteksti\'>' + kordinaattix + ', ' +
+          kordinaattiy +
+          '</span><br><br>' + '<b>Hinta:</b> ' + usagecost + '<br>' +
+          '<b>Tyyppi:</b> ' + tyyppi +
+          '<br><b>Käyttö:</b> ' + connectiontype + '<br>' + '<b>AC/DC:</b> ' +
+          acdc + '<br><br>' + käyttö + '<br><br> Määrä:' + pistokkeet + '<br>' +
+          '<br><a href="#" id="tismus"><div id=\"navigoi\">Navigoi</div></a>').
+      addTo(latauspaikatm).
+      on('click', function() {
 
-    // try {
-    L.marker([crd.latitude, crd.longitude], kuvake).addTo(map).bindPopup("<b>" + teksti + '</b><br> <span class="osoite">' + osoite + ", " + kaupunki + "</span><br>" +
-        "<span class='pienempiteksti'>" + kordinaattix + ", " + kordinaattiy + "</span><br><br>" + "<b>Hinta:</b> " + usagecost + "<br>" + "<b>Tyyppi:</b> " + tyyppi +
-        "<br><b>Käyttö:</b> " + connectiontype + "<br>" + "<b>AC/DC:</b> " + acdc + "<br><br>" + käyttö + "<br><br> Määrä:" + pistokkeet + "<br>" +
-        '<br><a href="#" id="tismus"><div id=\"navigoi\">Navigoi</div></a>').addTo(latauspaikatm)
-        .on("click", function () {
+        /*  try {
+              document.getElementById("nimi").innerHTML = latauspiste.AddressInfo.Title;
+              document.getElementById("osoite").innerHTML = latauspiste.AddressInfo.AddressLine1;
+              document.getElementById("kaupunki").innerHTML = latauspiste.AddressInfo.Town;
+              document.getElementById("lisatiedot").innerHTML = latauspiste.AddressInfo.AccessComments;
+              //hinta (kesken)
+              document.getElementById("hinta").innerHTML = latauspiste.UsageCost;
+              document.getElementById("provider").innerHTML = latauspiste.Connections[0].Level.Title;
+              document.getElementById('tismus').href = `https://www.google.com/maps/dir/?api=1&origin=${paikka.latitude},${paikka.longitude}&destination=${crd.latitude},${crd.longitude}&travelmode=driving`;
+          } catch (e) {
+              // console.error(e.message);
+          }*/
+        document.getElementById(
+            'tismus').href = `https://www.google.com/maps/dir/?api=1&origin=${paikka.latitude},${paikka.longitude}&destination=${crd.latitude},${crd.longitude}&travelmode=driving`;
 
-            /*  try {
-                  document.getElementById("nimi").innerHTML = latauspiste.AddressInfo.Title;
-                  document.getElementById("osoite").innerHTML = latauspiste.AddressInfo.AddressLine1;
-                  document.getElementById("kaupunki").innerHTML = latauspiste.AddressInfo.Town;
-                  document.getElementById("lisatiedot").innerHTML = latauspiste.AddressInfo.AccessComments;
-                  //hinta (kesken)
-                  document.getElementById("hinta").innerHTML = latauspiste.UsageCost;
-                  document.getElementById("provider").innerHTML = latauspiste.Connections[0].Level.Title;
-                  document.getElementById('tismus').href = `https://www.google.com/maps/dir/?api=1&origin=${paikka.latitude},${paikka.longitude}&destination=${crd.latitude},${crd.longitude}&travelmode=driving`;
-              } catch (e) {
-                  // console.error(e.message);
-              }*/
-            document.getElementById('tismus').href = `https://www.google.com/maps/dir/?api=1&origin=${paikka.latitude},${paikka.longitude}&destination=${crd.latitude},${crd.longitude}&travelmode=driving`;
-
-
-        })
-    // L.marker.style.filter = 'hue-rotate(180deg)';
-    //.openPopup();
-    //} catch(error){}
+      });
+  // L.marker.style.filter = 'hue-rotate(180deg)';
+  //.openPopup();
+  //} catch(error){}
 }
 
 // Funktio, joka ajetaan, jos paikkatietojen hakemisessa tapahtuu virhe
 function error(err) {
-    console.warn(`ERROR(${err.code}): ${err.message}`);
+  console.warn(`ERROR(${err.code}): ${err.message}`);
 }
 
 // Käynnistetään paikkatietojen haku
 navigator.geolocation.getCurrentPosition(success, error, options);
 
 //slideri
-let rangeslider = document.getElementById("sliderRange");
-let output = document.getElementById("demo");
+let rangeslider = document.getElementById('sliderRange');
+let output = document.getElementById('demo');
 output.innerHTML = rangeslider.value;
-output.value
+output.value;
 /*
 rangeslider.oninput = function arvoja(arvot) {
     arvot = output.innerHTML = this.value;
    //arvot.
     console.log(arvot);
-}
-
-let testailu = output.innerHTML;
+}*
+ */
 
 /*
 let rasmus =  document.getElementById("sliderRange").value
@@ -210,47 +219,54 @@ let rasmus = 1;
 //openchargemap
 function haeLatauspisteet(crd) {
 
-
-    fetch(
-        `https://api.openchargemap.io/v3/poi/?distanceunit=KM&distance=${rangeslider.value}&latitude=${crd.latitude}&longitude=${crd.longitude}&maxresults=250`).then(function (vastaus) {
+  fetch(
+      `https://api.openchargemap.io/v3/poi/?distanceunit=KM&distance=${rangeslider.value}&latitude=${crd.latitude}&longitude=${crd.longitude}&maxresults=250`).
+      then(function(vastaus) {
         console.log('palvelimen vastaus', vastaus);
         return vastaus.json();
         //appendData(vastaus);
-    })
-        .then(function (latauspisteet) {
-            console.log('tulos', latauspisteet);
+      }).
+      then(function(latauspisteet) {
+        console.log('tulos', latauspisteet);
 
-            for (let i = 0; i < latauspisteet.length; i++) {
+        for (let i = 0; i < latauspisteet.length; i++) {
 
-                const lpPaikka = {
-                    latitude: latauspisteet[i].AddressInfo.Latitude,
-                    longitude: latauspisteet[i].AddressInfo.Longitude,
-                    greenIcon,
-                };
+          const lpPaikka = {
+            latitude: latauspisteet[i].AddressInfo.Latitude,
+            longitude: latauspisteet[i].AddressInfo.Longitude,
+            greenIcon,
+          };
 
-                /*    for(let j = 0; j < latauspisteet[i].Connections[j].ConnectionType; j++) {
-                        const taulukko = [j];
-                        console.log(taulukko);
-                    }*/
-                try {
-                    lisaaMarker(lpPaikka, latauspisteet[i], {icon: greenIcon}, latauspisteet[i].AddressInfo.Title, latauspisteet[i].UsageCost, latauspisteet[i].Connections[0].Level.Title,
-                        latauspisteet[i].AddressInfo.AddressLine1, latauspisteet[i].AddressInfo.Town, latauspisteet[i].AddressInfo.Latitude, latauspisteet[i].AddressInfo.Longitude,
-                        latauspisteet[i].Connections[0].ConnectionType.Title, latauspisteet[i].Connections[0].CurrentType.Title, latauspisteet[i].UsageType.Title, latauspisteet[i].NumberOfPoints);
+          /*    for(let j = 0; j < latauspisteet[i].Connections[j].ConnectionType; j++) {
+                  const taulukko = [j];
+                  console.log(taulukko);
+              }*/
+          try {
+            lisaaMarker(lpPaikka, latauspisteet[i], {icon: greenIcon},
+                latauspisteet[i].AddressInfo.Title, latauspisteet[i].UsageCost,
+                latauspisteet[i].Connections[0].Level.Title,
+                latauspisteet[i].AddressInfo.AddressLine1,
+                latauspisteet[i].AddressInfo.Town,
+                latauspisteet[i].AddressInfo.Latitude,
+                latauspisteet[i].AddressInfo.Longitude,
+                latauspisteet[i].Connections[0].ConnectionType.Title,
+                latauspisteet[i].Connections[0].CurrentType.Title,
+                latauspisteet[i].UsageType.Title,
+                latauspisteet[i].NumberOfPoints);
 
-                } catch (error) {
+          } catch (error) {
 
-                }
-            }
+          }
+        }
 
-        })
-        .catch(function (error) {
-            console.log(error);
-        });
+      }).
+      catch(function(error) {
+        console.log(error);
+      });
 }
 
-
 //hakukenttä
-const nappi = document.getElementById("nappi");
+const nappi = document.getElementById('nappi');
 const input2 = document.getElementById('haku');
 console.log(input2.value);
 /*
@@ -263,31 +279,30 @@ httprequest.withCredentials = true;
 httprequest.setRequestHeader("Content-Type", "application/json");
 httprequest.send({ 'request': "authentication token" });
 */
-nappi.addEventListener('click', function () {
-    //console.log("nappia painettiin.")
-    fetch('https://nominatim.openstreetmap.org/search/' + input2.value)
-        .then(function (paikka) {
-            return paikka.json();
-        })
-        .catch(function (error) {
-            console.log(error);
-        });
-    console.log(paikka);
+nappi.addEventListener('click', function() {
+  //console.log("nappia painettiin.")
+  fetch('https://nominatim.openstreetmap.org/search/' + input2.value).
+      then(function(paikka) {
+        return paikka.json();
+      }).
+      catch(function(error) {
+        console.log(error);
+      });
+  console.log(paikka);
 });
 
 //enter näppäimellä haku
 
-
 // Execute a function when the user releases a key on the keyboard
-input2.addEventListener('keyup', function (event) {
-    // Number 13 is the "Enter" key on the keyboard
-    if (event.keyCode === 13) {
-        // Cancel the default action, if needed
-        event.preventDefault();
-        // Trigger the button element with a click
-        document.getElementById('nappi').click();
-        console.log("toimii");
-    }
+input2.addEventListener('keyup', function(event) {
+  // Number 13 is the "Enter" key on the keyboard
+  if (event.keyCode === 13) {
+    // Cancel the default action, if needed
+    event.preventDefault();
+    // Trigger the button element with a click
+    document.getElementById('nappi').click();
+    console.log('toimii');
+  }
 
 });
 
@@ -307,7 +322,3 @@ function appendData(vastaus) {
 }
 
  */
-
-function huomautus() {
-    alert("Tiedot tulevat OpenChargeMap API:sta. Emme vastaa tietojen oikeellisuudesta. Yhteydenotot: timojuhani.hypponen@gmail.com");
-}
